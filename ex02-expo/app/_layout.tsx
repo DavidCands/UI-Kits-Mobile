ee4792efcf90aae7931f122ad505c724a9d848f1
DavidCands <davidcandido1914@gmail.com>
2025-10-30 17:55:49 -0300

    Corrigindo erro da TaskList

--- ex02-expo/app/_layout.tsx
+++ ex02-expo/app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
 import { Provider as PaperProvider } from "react-native-paper";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function RootLayout() {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <PaperProvider>
-      <Stack />
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <Stack />
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
